e3e5c3745a857ca68635b979c325ed391c239d47
sagar1861 <skarmakar1861995@gmail.com>
2025-02-11 13:22:58 +0530

    closure.js file added in JS_Practice folder and deleted the old report folder

---
+++ test/JS_Practice1/Closure.js
@@ -0,0 +1,43 @@
+
+
+function createCounter(){
+    let count = 0;
+
+    return {
+        increment : function (){
+            count = count+1
+            return count
+            // console.log(count);
+            
+        },
+        decrement : function(){
+            count = count - 1
+            return count;
+        },
+        getCount : function(){
+            return count;
+        }
+    }
+
+}
+
+let counter = createCounter()
+// console.log(counter); /*{
+//     increment: [Function: increment],
+//     decrement: [Function: decrement],
+//     getCount: [Function: getCount]
+//   }*/
+
+console.log("counter.increment() : ",counter.increment()); //1
+console.log("counter.increment() : ",counter.increment()); //2
+console.log("counter.increment() : ",counter.increment()); //3
+
+
+
+console.log("counter.decrement() : ",counter.decrement()); // 2
+console.log("counter.decrement() : ",counter.decrement()); // 1
+
+
+console.log("counter.getCount() : ",counter.getCount()); //1
+
+
