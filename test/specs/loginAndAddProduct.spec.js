a7370a7522876f4e887783f48d8c4f40e4f457ec
sagar1861 <skarmakar1861995@gmail.com>
2025-02-12 15:07:09 +0530

    1 spec and 1 screen files are added

---
+++ test/specs/loginAndAddProduct.spec.js
@@ -0,0 +1,16 @@
+import loginPage from "../pageobjects/loginPrivate.screen.js";
+import { assert,expect } from "chai";
+import data from '../testData/allDataImport.js'
+import mainPageScreen from "../pageobjects/mainPage.screen.js";
+
+describe('Login and Add  Product to cart Test', () => {
+
+    it('should login with valid credentials', async () => {
+        await loginPage.login(data.loginData.username, data.loginData.password);
+
+        expect(await browser.getUrl()).to.include('inventory.html')
+        await mainPageScreen.verifyProductsDisplayed();
+
+        await mainPageScreen.addFirstProductToCart();
+    });
+});
