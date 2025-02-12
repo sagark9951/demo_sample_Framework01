b05b41f4c2bdee2d783e0978c2cc6af6f92e46a8
sagar1861 <skarmakar1861995@gmail.com>
2025-02-12 14:51:43 +0530

    loginAndAddProduct spec file and mainPage.screen file added and .gitattribute file added

--- test/pageobjects/homepage.screen.js
+++ test/pageobjects/homepage.screen.js
@@ -8,25 +8,7 @@ const homePage = {
 
 class HomePage {
 
-    get usernameInput() { return $('#user-name'); }
-    get passwordInput() { return $('#password'); }
-    get loginButton() { return $('#login-button'); }
-    // get errorMessage() { return $('.error-message-container'); }
 
-    async login(username, password) {
-        await this.usernameInput.setValue(username);
-        await reporter.stepLevelLog(`Entered ${username} in User input field`)
-
-        await this.passwordInput.setValue(password);
-        await reporter.stepLevelLog(`Entered ${password} in password input field`)
-
-        await this.loginButton.click();
-        await reporter.stepLevelLog(`Clicked on Login Button`)
-    }
-
-    async errorMssg(){
-        return await $(loginPage.errorMessage).isDisplayed()
-    }
 }
 
 export default new HomePage();
